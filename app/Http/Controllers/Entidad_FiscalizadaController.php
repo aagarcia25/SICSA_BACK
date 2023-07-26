8e887dc77f7c27d5f6bb28af28484ea48d19aeb2
EdithLizcano <126712189+EdithLizcano@users.noreply.github.com>
2023-07-26 18:02:52 -0500

    Update Entidad_FiscalizadaController.php
    Corrección de cat entidad fiscalizada, agregar, editar.

--- app/Http/Controllers/Entidad_FiscalizadaController.php
+++ app/Http/Controllers/Entidad_FiscalizadaController.php
@@ -31,7 +31,7 @@ class Entidad_FiscalizadaController extends Controller
 
                     $OBJ->ModificadoPor = $request->CHUSER;
                     $OBJ->CreadoPor = $request->CHUSER;
-                    $OBJ->anio = $request->NOMBRE;
+                    $OBJ->Descripcion = $request->DESCRIPCION;
                     $OBJ->save();
                     $response = $OBJ;
     
@@ -40,7 +40,7 @@ class Entidad_FiscalizadaController extends Controller
     
                     $OBJ = CatEntidadFiscalizada::find($request->CHID);
                     $OBJ->ModificadoPor = $request->CHUSER;
-                    $OBJ->Nombre = $request->NOMBRE;
+                    //$OBJ->Nombre = $request->NOMBRE;
                     $OBJ->Descripcion = $request->DESCRIPCION;
                     $OBJ->save();
                     $response = $OBJ;
