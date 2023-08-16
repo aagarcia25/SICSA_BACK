56c10c321e7450ccda5d65fb2ea525f8b635875c
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-08-16 10:26:37 -0600

    Update AccionesController.php

--- app/Http/Controllers/AccionesController.php
+++ app/Http/Controllers/AccionesController.php
@@ -8,71 +8,67 @@ use Illuminate\Support\Facades\DB;
 
 class AccionesController extends Controller
 {
-     /* SE IDENTIFICA EL TIPO DE OPERACION A REALIZAR
-         1._ INSERTAR UN REGISTRO
-         2._ ACTUALIZAR UN REGISTRO
-         3._ ELIMINAR UN REGISTRO
-         4._ CONSULTAR GENERAL DE REGISTROS, (SE INCLUYEN FILTROS)
-        */
+    /* SE IDENTIFICA EL TIPO DE OPERACION A REALIZAR
+    1._ INSERTAR UN REGISTRO
+    2._ ACTUALIZAR UN REGISTRO
+    3._ ELIMINAR UN REGISTRO
+    4._ CONSULTAR GENERAL DE REGISTROS, (SE INCLUYEN FILTROS)
+     */
 
-        public function Acciones_index(Request $request)  {
-        
-            $SUCCESS = true;
-            $NUMCODE = 0;
-            $STRMESSAGE = 'Exito';
-            $response = "";
+    public function Acciones_index(Request $request)
+    {
 
+        $SUCCESS = true;
+        $NUMCODE = 0;
+        $STRMESSAGE = 'Exito';
+        $response = "";
 
-            try {
-                $type = $request->NUMOPERACION;
-    
-                if ($type == 1) {
-                    $OBJ = new Accione();
-                    $OBJ->FechaCreacion= $request->CHUSER;
-                    $OBJ->ModificadoPor= $request->CHUSER;
-                    $OBJ->CreadoPor= $request->CHUSER;
-                    $OBJ->anio= $request->CHUSER;
-                    $OBJ->idTipoAccion= $request->CHUSER;
-                    $OBJ->idAuditoria= $request->CHUSER;
-                    $OBJ->idEstatusAccion= $request->CHUSER;
-                    $OBJ->ClaveAccion= $request->CHUSER;
-                    $OBJ->TextoAccion= $request->CHUSER;
-                    $OBJ->Valor= $request->CHUSER;
-                    $OBJ->UltimaActualizacion= $request->CHUSER;
-                    $OBJ->save();
-                    $response = $OBJ;
-    
-    
-                } else if ($type == 2) {
-    
-                    $OBJ = Accione::find($request->CHID);
-                    $OBJ->FechaCreacion= $request->CHUSER;
-                    $OBJ->ModificadoPor= $request->CHUSER;
-                    $OBJ->CreadoPor= $request->CHUSER;
-                    $OBJ->anio= $request->CHUSER;
-                    $OBJ->idTipoAccion= $request->CHUSER;
-                    $OBJ->idAuditoria= $request->CHUSER;
-                    $OBJ->idEstatusAccion= $request->CHUSER;
-                    $OBJ->ClaveAccion= $request->CHUSER;
-                    $OBJ->TextoAccion= $request->CHUSER;
-                    $OBJ->Valor= $request->CHUSER;
-                    $OBJ->UltimaActualizacion= $request->CHUSER;
-                    $OBJ->save();
-                    $response = $OBJ;
-    
-    
-                } else if ($type == 3) {
-                    $OBJ = Accione::find($request->CHID);
-                    $OBJ->deleted = 1;
-                    $OBJ->ModificadoPor = $request->CHUSER;
-                    $OBJ->save();
-                    $response = $OBJ;
-    
-    
-                } else if ($type == 4) {
+        try {
+            $type = $request->NUMOPERACION;
 
+            if ($type == 1) {
+                $OBJ = new Accione();
+                $OBJ->FechaCreacion = $request->CHUSER;
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->CreadoPor = $request->CHUSER;
+                $OBJ->anio = $request->CHUSER;
+                $OBJ->idTipoAccion = $request->CHUSER;
+                $OBJ->idAuditoria = $request->CHUSER;
+                $OBJ->idEstatusAccion = $request->CHUSER;
+                $OBJ->ClaveAccion = $request->CHUSER;
+                $OBJ->TextoAccion = $request->CHUSER;
+                $OBJ->Valor = $request->CHUSER;
+                $OBJ->UltimaActualizacion = $request->CHUSER;
+                $OBJ->save();
+                $response = $OBJ;
 
-                    $query = "
+            } elseif ($type == 2) {
+
+                $OBJ = Accione::find($request->CHID);
+                $OBJ->FechaCreacion = $request->CHUSER;
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->CreadoPor = $request->CHUSER;
+                $OBJ->anio = $request->CHUSER;
+                $OBJ->idTipoAccion = $request->CHUSER;
+                $OBJ->idAuditoria = $request->CHUSER;
+                $OBJ->idEstatusAccion = $request->CHUSER;
+                $OBJ->ClaveAccion = $request->CHUSER;
+                $OBJ->TextoAccion = $request->CHUSER;
+                $OBJ->Valor = $request->CHUSER;
+                $OBJ->UltimaActualizacion = $request->CHUSER;
+                $OBJ->save();
+                $response = $OBJ;
+
+            } elseif ($type == 3) {
+                $OBJ = Accione::find($request->CHID);
+                $OBJ->deleted = 1;
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->save();
+                $response = $OBJ;
+
+            } elseif ($type == 4) {
+
+                $query = "
                     SELECT
                       accion.id,
                       accion.deleted,
@@ -92,35 +88,31 @@ class AccionesController extends Controller
                       cea.Descripcion AS DescripcionEstatusAccion
 
                       FROM SICSA.Acciones accion
-                      LEFT JOIN SICSA.Auditoria aud ON accion.idAuditoria = aud.id
+                      LEFT JOIN SICSA.auditoria aud ON accion.idAuditoria = aud.id
                       LEFT JOIN SICSA.Cat_Tipos_Accion cta ON accion.idTipoAccion = cta.id
-                      LEFT JOIN SICSA.Cat_Estatus_Acciones cea ON accion.idEstatusAccion = cea.id  
-                    where accion.deleted =0 
+                      LEFT JOIN SICSA.Cat_Estatus_Acciones cea ON accion.idEstatusAccion = cea.id
+                    where accion.deleted =0
 
                       ";
 
+                $query = $query . " and accion.idAuditoria='" . $request->P_IDAUDITORIA . "'";
 
-                    $query =  $query . " and accion.idAuditoria='" . $request->P_IDAUDITORIA  . "'";
+                $response = DB::select($query);
 
-                    $response = DB::select($query);
-
-                }
-            } catch (\Exception $e) {
-                $SUCCESS = false;
-                $NUMCODE = 1;
-                $STRMESSAGE = $e->getMessage();
             }
-           
-    
-    
-    
-            return response()->json(
-                [
-                 'NUMCODE' =>$NUMCODE,
-                 'STRMESSAGE' =>$STRMESSAGE,
-                 'RESPONSE' => $response,
-                 'SUCCESS' => $SUCCESS,
-                 ] );
-    
-     }
+        } catch (\Exception $e) {
+            $SUCCESS = false;
+            $NUMCODE = 1;
+            $STRMESSAGE = $e->getMessage();
+        }
+
+        return response()->json(
+            [
+                'NUMCODE' => $NUMCODE,
+                'STRMESSAGE' => $STRMESSAGE,
+                'RESPONSE' => $response,
+                'SUCCESS' => $SUCCESS,
+            ]);
+
+    }
 }
