bd66c5c6c85afb8e60591786c9f8df0921123e43
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-08-17 10:40:45 -0600

    ajustes

---
+++ app/Http/Controllers/PlandetrabajoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plandetrabajo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PlandetrabajoController extends Controller
+{
+    /* SE IDENTIFICA EL TIPO DE OPERACION A REALIZAR
+    1._ INSERTAR UN REGISTRO
+    2._ ACTUALIZAR UN REGISTRO
+    3._ ELIMINAR UN REGISTRO
+    4._ CONSULTAR GENERAL DE REGISTROS, (SE INCLUYEN FILTROS)
+     */
+
+    public function planindex(Request $request)
+    {
+
+        $SUCCESS = true;
+        $NUMCODE = 0;
+        $STRMESSAGE = 'Exito';
+        $response = "";
+
+        try {
+            $type = $request->NUMOPERACION;
+
+            if ($type == 1) {
+                $OBJ = new Plandetrabajo();
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->CreadoPor = $request->CHUSER;
+                $OBJ->Clave = $request->Clave;
+                $OBJ->Descripcion = $request->Descripcion;
+                $OBJ->idCatUnidadAdmin = $request->idCatUnidadAdmin;
+                $OBJ->save();
+                $response = $OBJ;
+
+            } elseif ($type == 2) {
+
+                $OBJ = Plandetrabajo::find($request->CHID);
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->Clave = $request->Clave;
+                $OBJ->Descripcion = $request->Descripcion;
+                $OBJ->idCatUnidadAdmin = $request->idCatUnidadAdmin;
+                $OBJ->save();
+                $response = $OBJ;
+
+            } elseif ($type == 3) {
+                $OBJ = Plandetrabajo::find($request->CHID);
+                $OBJ->deleted = 1;
+                $OBJ->ModificadoPor = $request->CHUSER;
+                $OBJ->save();
+                $response = $OBJ;
+
+            } elseif ($type == 4) {
+
+                $query = "
+                      SELECT
+                        pt.id,
+                        pt.UltimaActualizacion,
+                        pt.FechaCreacion,
+                        getUserName(pt.ModificadoPor) ModificadoPor,
+                        getUserName(pt.CreadoPor) CreadoPor,
+                        pt.start,
+                        pt.end,
+                        pt.name,
+                        pt.type,
+                        pt.progress,
+                        pt.idauditoria
+                       FROM SICSA.plandetrabajo pt
+                       WHERE pt.deleted =0
+
+                    ";
+
+                $query = $query . " and pt.idauditoria='" . $request->P_IDAUDITORIA . "'";
+                $query = $query . "  ORDER BY pt.FechaCreacion DESC";
+
+                $response = DB::select($query);
+            }
+        } catch (\Exception $e) {
+            $SUCCESS = false;
+            $NUMCODE = 1;
+            $STRMESSAGE = $e->getMessage();
+        }
+
+        return response()->json(
+            [
+                'NUMCODE' => $NUMCODE,
+                'STRMESSAGE' => $STRMESSAGE,
+                'RESPONSE' => $response,
+                'SUCCESS' => $SUCCESS,
+            ]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\MigraDataController;
 use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\OficiosAController;
 use App\Http\Controllers\Origen_AuditoriaController;
+use App\Http\Controllers\PlandetrabajoController;
 use App\Http\Controllers\RamoController;
 use App\Http\Controllers\SectorController;
 use App\Http\Controllers\SelectController;
@@ -58,5 +59,6 @@ Route::group([
     Route::post('SelectIndex', [SelectController::class, 'SelectIndex']);
     Route::post('OficiosA_index', [OficiosAController::class, 'OficiosA_index']);
     Route::post('areaindex', [Area_AuditoraController::class, 'areaindex']);
+    Route::post('planindex', [PlandetrabajoController::class, 'planindex']);
 
 });
