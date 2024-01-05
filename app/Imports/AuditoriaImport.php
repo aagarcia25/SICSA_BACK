611069d50a39dd9a6143ea79703da83fae2025d0
EdithLizcano <126712189+EdithLizcano@users.noreply.github.com>
2024-01-05 17:36:53 -0600

    alta de auditorias

--- app/Imports/AuditoriaImport.php
+++ app/Imports/AuditoriaImport.php
@@ -5,14 +5,21 @@ namespace App\Imports;
 use App\Models\Auditorium;
 use App\Models\Accione;
 use App\Models\CatModalidad;
-use App\Models\CatEstatusAccione;
+use App\Models\CatOrigenAuditorium;
+use App\Models\CatAnio;
+use App\Models\CatEstatusAuditorium;
+use App\Models\CatTipo;
+use App\Models\CatInicioAuditorium;
+use App\Models\CatGrupoFuncional;
+use App\Models\CatSector;
 use App\Models\CatTiposAccion;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 
-class AuditoriaImport extends Auditorium implements ToModel, WithHeadingRow, WithChunkReading, WithBatchInserts
+class AuditoriaImport extends Auditorium implements ToModel, WithHeadingRow, WithChunkReading, WithBatchInserts, WithCalculatedFormulas
 {
 
     public $userid;
@@ -34,8 +41,25 @@ class AuditoriaImport extends Auditorium implements ToModel, WithHeadingRow, Wit
         if(isset($row['id_modalidad'])){
 
              $idModalidadFromExcel = $row['id_modalidad'];
+             $id_origen_auditoriaFromExcel = $row['id_origen_auditoria'];
+             $id_estatusFromExcel = $row['id_estatus'];
+             //$anioFromExcel = $row['id_anio'];
+             $id_tipoFromExcel = $row['id_tipo'];
+             $id_inicio_auditoriaFromExcel = $row['id_inicio_auditoria'];
+             $id_grupo_funcionalFromExcel = $row['id_grupo_funcional'];
+             $id_sectorFromExcel = $row['id_sector'];
+
 
         $OBJ1 = CatModalidad::find($idModalidadFromExcel);
+        $OBJ2 = CatOrigenAuditorium::find($id_origen_auditoriaFromExcel);
+        $OBJ3 = CatEstatusAuditorium::find($id_estatusFromExcel);
+        //$OBJ4 = CatAnio::find($anioFromExcel);
+        $OBJ3 = CatTipo::find($id_tipoFromExcel);
+        $OBJ3 = CatInicioAuditorium::find($id_inicio_auditoriaFromExcel);
+        $OBJ3 = CatGrupoFuncional::find($id_inicio_auditoriaFromExcel);
+        $OBJ3 = CatSector::find($id_sectorFromExcel);
+
+
 
         if($OBJ1){
             return new Auditorium([
@@ -54,21 +78,19 @@ class AuditoriaImport extends Auditorium implements ToModel, WithHeadingRow, Wit
                 'universopesos' => $row['universo_pesos'],
                 'muestrapesos' => $row['muestra_pesos'],
                 'idModalidad' => $idModalidadFromExcel,
+                'idcatorigenaud' => $id_origen_auditoriaFromExcel,
+                'idEstatus' => $id_estatusFromExcel,
+                //'anio' => $anioFromExcel,
+                'idClasificacion' => $id_tipoFromExcel,
+                'idInicioauditoria' => $id_inicio_auditoriaFromExcel,
+                'idCatGrupoFuncional' => $id_grupo_funcionalFromExcel,
+                'idCatSector' => $id_sectorFromExcel,
 
-                // 'anio' => $row['ano_cuenta_publica'],
-                // 'NombreAudoria' => $row['clave_accion'],
-                // 'Encargado' => $row['texto_accion'],
-                // 'PersonalEncargado' => $row['accion_superveniente'],
+
 
             ]);
         }
-
-        }
-
-       
-            
-        
-
+        }         
     }
 
     public function headingRow(): int
